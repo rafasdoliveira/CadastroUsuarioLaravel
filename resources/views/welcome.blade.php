29b10b53bbe8a0f4e7c443c3192e6bbe330a6a7d
Hermano Uchoa <hermano.uchoa@icc.org.br>
2024-02-08 08:00:59 -0300

    use helper asset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.main')
 @section('style')
-<link rel="stylesheet" href="/css/global.css">
+
+{{-- Use a helper  asset em todas as referencias de css, js, imagens, fonts, etc--}}
+<link rel="stylesheet" href="{{ asset('/css/global.css') }}">
 <link rel="stylesheet" href="/css/welcome.css">
 @endsection
 
@@ -10,9 +12,10 @@
 <div class="welcome">
     <h2 class="learn__h2">
       Bem Vindo ao CodingExperience!
+      <p>Validação!</p>
     </h2>
     <p>Lugar onde você vai aprender com pessoas que já estão no mercado!</p>
 </div>
-@endsection    
+@endsection
 
-  
+
