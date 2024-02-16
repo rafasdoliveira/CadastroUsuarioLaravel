cd38423ec468bba3adcbd6ff0e66eddba4525ebc
Rafael Silva de Oliveira <rafasdoliveira@outlook.com>
2024-02-16 13:03:28 -0300

    Busca de usuarios

--- resources/views/usuarios/cadastrados.blade.php
+++ resources/views/usuarios/cadastrados.blade.php
@@ -14,15 +14,41 @@
                     <input type="text" name="nome_usuario" id="nome_usuario" placeholder="Digite o nome do usuário">
                     <div class="search__input-button">
                         <span></span>
-                        <button type="submit">Pesquisar</button>
+                        <button class="button" type="submit">Pesquisar</button>
                     </div>
                 </div>
             </div>
         </form>
-        @if (isset($usuario))
-            <div class="profile">
-                <p>{{ $usuario->usuario }}</p>
+
+
+        <div class="profile">
+            <div class="profile__img">
+                @if (isset($usuario))
+                    {{ $usuario->imagem }}
+                @endif
             </div>
-        @endif
+            <div class="profile__dados">
+                <div class="header">
+                    <div class="usuarios">
+                        @if (isset($usuario))
+                            {{ $usuario->primeiroNome }}
+                        @endif
+                        @if (isset($usuario))
+                            {{ $usuario->ultimoNome }}
+                        @endif
+                        <div class="usuario">
+                            @if (isset($usuario))
+                            {{ $usuario->usuario }}
+                            @endif
+                        </div>
+                    </div>
+                    <div class="created">
+                        @if (isset($usuario))
+                            {{ $usuario->created_at }}
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
